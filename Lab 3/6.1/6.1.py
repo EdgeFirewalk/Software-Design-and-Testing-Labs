01b82c433570c4af063928849da740b1f43e21df
EdgeFirewalk <thedinsorpaswer2@mail.ru>
2024-03-07 21:16:54 +0600

    Add second task

--- Lab 3/6.1/6.1.py
+++ Lab 3/6.1/6.1.py
@@ -4,22 +4,21 @@
 # Задание 6.1
 
 def fibonacci(n):
-    fib = [0, 1]
-    while fib[-1] < n:
-        fib.append(fib[-1] + fib[-2])
-    return fib
+    fib = [0, 1] # Инициализация списка с первыми двумя числами Фибоначчи
+    while fib[-1] < n: # Пока последнее число в списке меньше n
+        fib.append(fib[-1] + fib[-2]) # Добавляем следующее число Фибоначчи
+    return fib # Возвращаем список чисел Фибоначчи
 
 def count_fibonacci_in_range(a, b):
-    fib_numbers = fibonacci(b)
-    return len([num for num in fib_numbers if a <= num <= b])
+    fib_numbers = fibonacci(b) # Получаем список чисел Фибоначчи до b
+    return len([num for num in fib_numbers if a <= num <= b]) # Возвращаем количество чисел в диапазоне [a, b]
 
 def main():
-    with open('input.txt', 'r') as file:
-        for line in file:
-            a, b = map(int, line.strip().split())
-            if a == b == 0:
+    with open('input.txt', 'r') as file: # Открываем файл для чтения
+        for line in file: # Читаем файл построчно
+            a, b = map(int, line.strip().split()) # Разбиваем строку на числа a и b
+            if a == b == 0: # Если оба числа равны 0, прерываем цикл
                 break
-            print(count_fibonacci_in_range(a, b))
+            print(count_fibonacci_in_range(a, b)) # Выводим количество чисел Фибоначчи в диапазоне [a, b]
 
-if __name__ == "__main__":
-    main()
+main()
