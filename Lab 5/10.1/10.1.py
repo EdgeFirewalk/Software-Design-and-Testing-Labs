c5bd52e85cda26e2b8ae1de4a1c02989cf993dea
EdgeFirewalk <thedinsorpaswer2@mail.ru>
2024-03-27 22:27:48 +0600

    Add lab 5 solutions + report (no diagrams)

---
+++ Lab 5/10.1/10.1.py
@@ -0,0 +1,69 @@
+import sys
+
+def init():
+    global p, r
+    p = [i for i in range(51)]
+    r = [1 for _ in range(51)]
+
+def find(x):
+    if x == p[x]:
+        return x
+    p[x] = find(p[x])
+    return p[x]
+
+def joint(x, y):
+    x = find(x)
+    y = find(y)
+    if x != y:
+        if r[x] > r[y]:
+            r[x] += r[y]
+            p[y] = x
+        else:
+            r[y] += r[x]
+            p[x] = y
+        return 1
+    return 0
+
+def dfs(node):
+    for i in range(1, 51):
+        if color_map[node][i]:
+            color_map[node][i] -= 1
+            color_map[i][node] -= 1
+            dfs(i)
+            print(i, node)
+
+with open('input.txt', 'r') as f:
+    t = int(f.readline().strip())
+    test_case = 0
+    while t > 0:
+        t -= 1
+        n = int(f.readline().strip())
+        color_map = [[0 for _ in range(51)] for _ in range(51)]
+        deg = [0 for _ in range(51)]
+        init()
+        for _ in range(n):
+            x, y = map(int, f.readline().strip().split())
+            color_map[x][y] += 1
+            color_map[y][x] += 1
+            deg[x] += 1
+            deg[y] += 1
+            joint(x, y)
+        print("Case #{}".format(test_case + 1))
+        test_case += 1
+        st = -1
+        flag = 0
+        for i in range(1, 51):
+            if deg[i] > 0:
+                st = i
+                if deg[i] & 1:
+                    flag = 1
+        for i in range(1, 51):
+            if deg[i] > 0:
+                if find(st) != find(i):
+                    flag = 1
+        if flag:
+            print("Impossibleee")
+        else:
+            dfs(st)
+        if t > 0:
+            print()
---
+++ Lab 5/9.1/9.1.py
@@ -0,0 +1,47 @@
+import sys
+
+class Bus:
+    def __init__(self, u, v, limit):
+        self.u = u
+        self.v = v
+        self.limit = limit
+
+def get_group(groups, u):
+    if groups[u] == u:
+        return u
+    return get_group(groups, groups[u])
+
+def main(input_file):
+    Case = 1
+    with open(input_file, 'r') as f:
+        for line in f:
+            N, R = map(int, line.split())
+            if N == 0 and R == 0:
+                break
+
+            buses = []
+            groups = list(range(N + 1))
+
+            for i in range(R):
+                bus = Bus(*map(int, f.readline().split()))
+                bus.limit -= 1  # Мистеру Ж тоже нужно место
+                buses.append(bus)
+
+            S, D, T = map(int, f.readline().split())
+
+            buses.sort(key=lambda s: s.limit, reverse=True)
+
+            for i in range(R):
+                bus = buses[i]
+                group_u = get_group(groups, bus.u)
+                group_v = get_group(groups, bus.v)
+                if group_u != group_v:
+                    groups[group_v] = group_u
+                    if get_group(groups, S) == get_group(groups, D):
+                        print(f"Scenario #{Case}\n"
+                              f"Minimum Number of Trips = {(T + bus.limit - 1) // bus.limit}\n")
+                        Case += 1
+                        break
+
+if __name__ == "__main__":
+    main("input.txt")
